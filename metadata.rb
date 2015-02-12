1578176d9e8a5a7b426aa666e278ef2a0d5a27c7
Stefano Harding <riddopic@gmail.com>
2015-02-12 08:05:03 -0800

    Set version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'sharding@trace3.com'
 license          'Apache 2.0'
 description      'Installs/Configures Oracle Directory Server Enterprise Edition'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'chef_handler', '>= 1.1.6'
 depends 'garcon',       '~> 0.8.3'
