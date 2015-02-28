5d33915ad496917467e570fd10385d4196b6959a
Stefano Harding <riddopic@gmail.com>
2015-02-27 18:55:48 -0800

    pessamistic versioning of cookbooks

--- metadata.rb
+++ metadata.rb
@@ -5,13 +5,16 @@ maintainer       'Stefano Harding'
 maintainer_email 'sharding@trace3.com'
 license          'Apache 2.0'
 description      'Installs/Configures Oracle Directory Server Enterprise Edition'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.1'
 
-depends 'chef_handler', '>= 1.1.6'
-depends 'garcon',       '~> 0.8.3'
+supports 'centos',      '>= 6.0'
+supports 'oracle',      '>= 6.0'
+supports 'redhat',      '>= 6.0'
+supports 'scientific',  '>= 6.0'
 
-supports 'centos'
-supports 'oracle'
-supports 'redhat'
-supports 'scientific'
+# Pessimistic versioning of cookbooks is specifically done to prevent any
+# possible variation in cookbook versions.
+#
+depends 'chef_handler', '~> 1.1.6'
+depends 'garcon',       '~> 0.8.5'
